df4fa1deea0c52d1bb5d4a60b94d15ff369c98a7
martinomaher <martinomaher@gmail.com>
2024-08-16 01:14:57 +0300

    Sheet 2 Problem B

---
+++ Sheet2ProblemB.cpp
@@ -0,0 +1,39 @@
+
+#include <iostream>
+
+using namespace std;
+
+
+bool IsEven(int number) {
+	return (number % 2 == 0);
+}
+
+bool ValidateNumberInBetween(int number, int from, int to) {
+	return (number >= from && number <= to);
+}
+
+int ReadNumberInRange(string message, int from, int to) {
+	int number;
+
+	do
+	{
+		cout << message;
+		cin >> number;
+
+	} while (!ValidateNumberInBetween(number, from, to));
+
+	return number;
+}
+
+
+int main()
+{
+	int number = ReadNumberInRange("enter number : ", 1, 1000);
+
+	for (int i = 1; i <= number; i++) {
+		if (IsEven(i)) {
+			cout << i << endl;
+		}
+	}
+}
+
